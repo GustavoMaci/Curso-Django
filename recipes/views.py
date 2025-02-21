c2d4b6bb2ba582d5f76f2796351364e5a58ec491
Gustavo Maciel <gustavohcmaciel15@gmail.com>
2025-02-21 16:59:43 -0300

    mudando view

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Luiz Otávio',
+        'name': 'Gustavo Henrique',
     })
